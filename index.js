78f979cc0d956f882785216ee91c228f023e7c8e
Rizvimahbub <rizvialam007@gmail.com>
2022-05-08 18:25:12 +0600

    JWT  section complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -10,58 +11,88 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyToken = (req, res, next) => {
+    const headers = req.headers.authorization;
+    if (!headers) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = headers.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Access Forbidden' })
+        }
+        else {
+            console.log('decoded', decoded)
+            req.decoded = decoded;
+            next()
+        }
+    })
 
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@warehouse.4ef9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serverDisplayCollection = client.db('Warehouse').collection('Display');
         const serverInventoryCollection = client.db('Warehouse').collection('Inventory');
         const serverOrdersCollection = client.db('Warehouse').collection('Orders');
 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '14d'
+            })
+            res.send({ token })
+        })
 
-        app.get('/bike', async(req, res) => {
+
+
+        //Display Section
+        app.get('/bike', async (req, res) => {
             const query = {};
             const cursor = serverDisplayCollection.find(query);
             const bikes = await cursor.toArray();
             res.send(bikes);
         })
 
-        app.get('/bike/:id', async(req, res) => {
+        app.get('/bike/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const bike = await serverDisplayCollection.findOne(query);
             res.send(bike);
         })
 
-        app.put('/bike/:id', async(req, res) => {
+        app.put('/bike/:id', async (req, res) => {
             const id = req.params.id;
             const bike = req.body;
-            const filter = {_id:ObjectId(id)};
-            const options = {upsert : true};
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const newDoc = {
-                $set : {
-                    quantity : bike.quantity,
+                $set: {
+                    quantity: bike.quantity,
                 }
             }
-            const result = await serverDisplayCollection.updateOne(filter,newDoc,options);
+            const result = await serverDisplayCollection.updateOne(filter, newDoc, options);
             res.send(result)
 
         })
 
 
-        app.post('/inventory', async(req, res) => {
+        //Inventory Section
+
+
+        app.post('/inventory', async (req, res) => {
             const bike = req.body;
             const finalResult = await serverInventoryCollection.insertOne(bike);
             res.send(finalResult);
         })
 
 
-        app.get('/inventory', async(req, res) => {
+        app.get('/inventory', async (req, res) => {
             const query = {};
             const items = serverInventoryCollection.find(query);
             const finalResult = await items.toArray();
@@ -69,54 +100,60 @@ async function run(){
         })
 
 
-        app.delete('/inventory/:id', async(req, res) => {
+        app.delete('/inventory/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const deleteItem = await serverInventoryCollection.deleteOne(query);
             res.send(deleteItem)
         })
 
-        app.put('/orders/:id', async(req, res) => {
+
+        //Order Section
+
+        app.put('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const email = req.query.email;
             const bike = req.body;
-            const filter = {_id:ObjectId(id)};
-            const options = {upsert : true};
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
             const newDoc = {
-                $set : {
-                    name : bike.name,
-                    url : bike.url,
-                    description : bike.description,
-                    supplier : bike.supplier,
-                    quantity : bike.quantity,
-                    email : bike.email
+                $set: {
+                    name: bike.name,
+                    url: bike.url,
+                    description: bike.description,
+                    supplier: bike.supplier,
+                    quantity: bike.quantity,
+                    email: bike.email
                 }
             }
-            const finalResult = await serverOrdersCollection.updateOne(filter,newDoc,options);
+            const finalResult = await serverOrdersCollection.updateOne(filter, newDoc, options);
             res.send(finalResult)
 
         })
 
 
-        app.get('/orders', async(req, res) => {
+        app.get('/orders', verifyToken, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            console.log(email)
-            const items = {email : email};
-            const cursor = serverOrdersCollection.find(items);
-            const finalResult = await cursor.toArray()
-            res.send(finalResult);
+            if (email === decodedEmail) {
+                const items = { email: email };
+                const cursor = serverOrdersCollection.find(items);
+                const finalResult = await cursor.toArray()
+                res.send(finalResult);
+            }
+
         })
 
 
-        app.delete('/orders/:id', async(req, res) => {
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const deleteItem = await serverOrdersCollection.deleteOne(query);
             res.send(deleteItem)
         })
 
     }
-    finally{
+    finally {
 
     }
 }
